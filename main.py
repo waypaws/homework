e3fb6ee136b33d9b662a20e16349ee915dae5d02
waypaws <zavadskiy-g@mail.ru>
2024-06-28 05:24:02 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# 1st program
+print(9 ** 0.5 * 5)
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3rd program
+print(1234 % 1211 + 5678 % 5611)
+# 4th program
+print(13 == (round(42.13-42, 2)*100))
